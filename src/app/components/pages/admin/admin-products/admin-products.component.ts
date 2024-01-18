cc029307cd5c53ec415654132801c4d444b19343
truongpham1309 <truong13092002@gmail.com>
2024-01-18 13:21:16 +0700

    search products title

--- src/app/components/pages/admin/admin-products/admin-products.component.ts
+++ src/app/components/pages/admin/admin-products/admin-products.component.ts
@@ -1,53 +1,66 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../../types/products';
 import { ProductsService } from '../../../../services/products.service';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
+import Fuse from 'fuse.js';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { debounce } from 'lodash';
+
 
 @Component({
   selector: 'app-admin-products',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './admin-products.component.html',
-  styleUrl: './admin-products.component.css'
+  styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
   products: Product[] = [];
-  idPr: string = "";
-  edit: boolean = false;
-  add: boolean = false;
+  productsInital: Product[] = [];
+  filteredProducts: Product[] = [];
+  keywords: string = "";
+  fuse!: Fuse<Product>;
 
-  constructor(private Product: ProductsService, private route: ActivatedRoute ) { }
+  constructor(private productService: ProductsService, private toastr: ToastrService) { }
+
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      if(params.get("edit")) this.edit = true;
-      if(params.get("add")) this.add = true;
-    })
-    this.Product.getAllProducts().subscribe(data => {
+    this.productService.getAllProducts().subscribe(data => {
       this.products = data;
-      if(this.add) this.toastr.success("Create product successfully!", "CREATE PRODUCT");
-      if(this.edit) this.toastr.success("Edit product successfully!", "EDIT PRODUCT");
-    })
+      this.productsInital = data;
+      this.fuse = new Fuse(this.products, { keys: ['title'], threshold: 0.3 });
+    });
   }
-  toastr = inject(ToastrService);
+
+  handelChangeInputSearchProductDebounce = debounce(() => {
+    if (!this.fuse) {
+      this.fuse = new Fuse(this.products, { keys: ['title'], threshold: 0.3 });
+    }
+
+    this.keywords = this.keywords.trim();
+
+    this.filteredProducts = this.keywords
+      ? this.fuse.search(this.keywords).map(result => result.item)
+      : this.productsInital;
+
+    this.products = this.filteredProducts;
+    console.log(this.products);
+
+  }, 500)
+
+  handelChangeInputSearchProduct(): void {
+    this.handelChangeInputSearchProductDebounce();
+  }
+
   handleRemoveProduct(id: string): void {
     if (!confirm('Are you sure you want to remove?')) return;
 
-    this.Product.removeProduct([id]).subscribe(
+    this.productService.removeProduct([id]).subscribe(
       () => {
         this.products = this.products.filter(p => p._id !== id);
-        // alert('Product removed successfully.');
         this.toastr.success("Product removed successfully!", "Success!");
-
-        // Gọi các bước cần thiết sau khi xóa sản phẩm thành công
-      },
-      (error) => {
-        console.error('Error removing product:', error);
-        // Xử lý lỗi nếu cần
       }
     );
-
   }
-
 }
