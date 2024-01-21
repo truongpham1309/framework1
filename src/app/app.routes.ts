4a573e8fc078e6b03e41c336b9df1f18a890a13f
truongpham1309 <truong13092002@gmail.com>
2024-01-21 20:21:46 +0700

    login UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { HomePageComponent } from './components/pages/clients/home-page/home-pag
 import { ProductsListComponent } from './components/pages/clients/products-list/products-list.component';
 import { ProductDetailComponent } from './components/pages/clients/product-detail/product-detail.component';
 import { NotFoundComponent } from './components/pages/clients/not-found/not-found.component';
+import { RegisterComponent } from './components/pages/clients/register/register.component';
 
 export const routes: Routes = [
     {
@@ -42,6 +43,10 @@ export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent,
+    },
+    {
+        path: "signup",
+        component: RegisterComponent,
     },
     {
         path: "",
