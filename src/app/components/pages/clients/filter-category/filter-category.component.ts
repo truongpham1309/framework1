848f513e603cb837d4099a3252525936fc2f76fd
truongpham1309 <truong13092002@gmail.com>
2024-01-30 20:14:09 +0700

    ASM 2

--- src/app/components/pages/clients/filter-category/filter-category.component.ts
+++ src/app/components/pages/clients/filter-category/filter-category.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../../../types/products';
+import { CategoryService } from '../../../../services/category.service';
 
 @Component({
   selector: 'app-filter-category',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './filter-category.component.html',
   styleUrl: './filter-category.component.css'
 })
-export class FilterCategoryComponent {
+export class FilterCategoryComponent implements OnInit {
+
+  categories: Category[] = [];
+  constructor(private cate: CategoryService) {
+  }
+  ngOnInit(): void {
+    this.cate.getAllCategories().subscribe(data => this.categories = data);
+  }
+  isDisplayBlock: boolean = false;
+  handleClickDisplayed() {
+    this.isDisplayBlock = !this.isDisplayBlock;
+  }
 
 }
